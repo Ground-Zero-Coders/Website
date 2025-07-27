97391a7abfe890385fface5afe10f1201f395e56
smgite <79395370+smgite@users.noreply.github.com>
2025-07-27 17:00:48 +0530

    Update coreTeam.ts

--- data/coreTeam.ts
+++ data/coreTeam.ts
@@ -12,25 +12,25 @@ export const coreTeam: CoreTeamMember[] = [
     id: 'ct-001',
     name: 'Shivansh Saxena',
     position: 'President',
-    image: 'shivansh.jpg',
+    image: '/shivansh.jpg',
   },
   {
     id: 'ct-002',
     name: 'Sankalp Shukla',
     position: 'Vice President',
-    image: 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=300&h=300&fit=crop',
+    image: '/sankalp2.jpg',
    },
   {
     id: 'ct-003',
     name: 'Aryan Tripathi',
     position: 'Tech CSE Head',
-    image: 'https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=300&h=300&fit=crop',
+    image: '/aryan.jpg',
    },
   {
     id: 'ct-004',
     name: 'Akshansh Kumar',
     position: 'Tech ECE Head',
-    image: 'https://images.pexels.com/photos/1181690/pexels-photo-1181690.jpeg?auto=compress&cs=tinysrgb&w=300&h=300&fit=crop',
+    image: '/akshansh.jpg',
   },
   {
     id: 'ct-005',
@@ -42,19 +42,19 @@ export const coreTeam: CoreTeamMember[] = [
     id: 'ct-006',
     name: 'Arein Jain',
     position: 'PR & Marketing Head',
-    image: 'https://images.pexels.com/photos/1181693/pexels-photo-1181693.jpeg?auto=compress&cs=tinysrgb&w=300&h=300&fit=crop',
+    image: '/arein.jpg',
   },
   {
     id: 'ct-006',
     name: 'Aarav Jai',
     position: 'Design & Creative Head',
-    image: 'https://images.pexels.com/photos/1181693/pexels-photo-1181693.jpeg?auto=compress&cs=tinysrgb&w=300&h=300&fit=crop',
+    image: '/aarav.jpg',
     },
    {
     id: 'ct-006',
     name: 'Paridhi Jain',
     position: '128 Head',
-    image: 'https://images.pexels.com/photos/1181693/pexels-photo-1181693.jpeg?auto=compress&cs=tinysrgb&w=300&h=300&fit=crop',
+    image: '/paridhi.jpg',
   },
   
 ];
